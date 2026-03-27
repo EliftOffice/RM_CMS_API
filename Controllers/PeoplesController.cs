8ff948e8361815a39b65102cb8dfb77538bede7a
prasanthkumar24 <24prasanthkumar@gmail.com>
2026-03-27 20:24:26 +0530

    Added new controller

---
+++ Controllers/PeoplesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RM_CMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeoplesController : ControllerBase
+    {
+    }
+}
