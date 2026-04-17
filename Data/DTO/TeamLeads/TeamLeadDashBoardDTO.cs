a860f978299e5d5ac23e002a7b14e57a27776b24
RMSairkishna <saikrishnayandrapati65@gmail.com>
2026-04-17 14:17:38 +0530

    done final

--- Data/DTO/TeamLeads/TeamLeadDashBoardDTO.cs
+++ Data/DTO/TeamLeads/TeamLeadDashBoardDTO.cs
@@ -14,6 +14,7 @@
 
     public class TeamLeadDTO
     {
+        public string? Name { get; set; }
         public string? TeamLeadId { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
